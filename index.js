116d7ebea202c57cb0f7259c68d28dbbcf46f8f4
MaxwellSorenGottesman <maxgman16@yahoo.com>
2024-07-23 20:53:51 -0400

    added phoneme blocks, added no close syllable varieties

--- index.js
+++ index.js
@@ -2,6 +2,286 @@
 const consonants = ["b","p","g","c","d","t","v","f","h","w","y","x","l","r","m","n","z","s","zj","sz"];
 const vowels = ["a","ā","e","ē","i","ī","u","ū","o"];
 
+const openConBlocks = [
+    ["w"],
+    ["r"],
+    ["t"],
+    ["y"],
+    ["p"],
+    ["s"],
+    ["d"],
+    ["f"],
+    ["g"],
+    ["h"],
+    ["zj"],
+    ["sz"],
+    ["l"],
+    ["x"],
+    ["c"],
+    ["v"],
+    ["b"],
+    ["n"],
+    ["m"],
+
+    ["t","w"],
+    ["t","r"],
+    ["t","y"],
+    ["t","s"],
+
+    ["p","w"],
+    ["p","w"],
+    ["p","y"],
+    ["p","l"],
+
+    ["s","w"],
+    ["s","r"],
+    ["s","t"],
+    ["s","p"],
+    ["s","l"],
+    ["s","c"],
+    ["s","n"],
+    ["s","m"],
+
+    ["d","w"],
+    ["d","r"],
+    ["d","y"],
+    
+    ["f","w"],
+    ["f","r"],
+    ["f","y"],
+    ["f","l"],
+    ["f","v"],
+
+    ["g","w"],
+    ["g","r"],
+    ["g","y"],
+    ["g","l"],
+    
+    ["h","w"],
+    ["h","y"],
+
+    ["zj","w"],
+    ["zj","y"],
+    ["zj","l"],
+    ["zj","v"],
+
+    ["sz","w"],
+    ["sz","y"],
+    ["sz","p"],
+    ["sz","f"],
+    ["sz","l"],
+    ["sz","c"],
+    ["sz","n"],
+    ["sz","m"],
+
+    ["l","w"],
+    ["l","y"],
+
+    ["x","w"],
+    ["x","r"],
+    ["x","y"],
+
+    ["c","w"],
+    ["c","r"],
+    ["c","y"],
+    ["c","l"],
+
+    ["v","w"],
+    ["v","r"],
+    ["v","y"],
+    ["v","l"],
+
+    ["b","w"],
+    ["b","r"],
+    ["b","y"],
+    ["b","l"],
+
+    ["n","w"],
+    ["n","y"],
+
+    ["m","w"],
+    ["m","r"],
+    ["m","y"],
+    ["m","l"],
+];
+
+const vowelBlocks = [
+    ["a"],
+    ["ā"],
+    ["e"],
+    ["ē"],
+    ["i"],
+    ["ī"],
+    ["u"],
+    ["ū"],
+    ["o"],
+
+    ["a","i"],
+    ["a","u"],
+    ["a","o"],
+
+    ["e","i"],
+
+    ["i","a"],
+    ["i","e"],
+    ["i","ē"],
+    ["i","u"],
+
+    ["o","i"]
+
+];
+
+const closeConBlocks = [
+    ["r"],
+    ["t"],
+    ["p"],
+    ["s"],
+    ["d"],
+    ["f"],
+    ["g"],
+    ["h"],
+    ["zj"],
+    ["sz"],
+    ["l"],
+    ["x"],
+    ["c"],
+    ["v"],
+    ["b"],
+    ["n"],
+    ["m"],
+    ["z"],
+
+    ["r","p"],
+    ["r","s"],
+    ["r","z"],
+    ["r","d"],
+    ["r","f"],
+    ["r","g"],
+    ["r","zj"],
+    ["r","sz"],
+    ["r","x"],
+    ["r","c"],
+    ["r","v"],
+    ["r","b"],
+    ["r","n"],
+    ["r","m"],
+
+    ["t","s"],
+    ["r","z"],
+
+    ["p","s"],
+    ["r","z"],
+    ["p","d"],
+    ["p","t"],
+
+    ["s","t"],
+    ["s","p"],
+    ["s","d"],
+    ["s","g"],
+    ["s","c"],
+
+    ["z","t"],
+    ["z","p"],
+    ["z","d"],
+    ["z","c"],
+
+    ["d","s"],
+    ["d","z"],
+
+    ["f","t"],
+    ["f","s"],
+    ["f","z"],
+    ["f","d"],
+
+    ["g","s"],
+    ["g","z"],
+    ["g","d"],
+
+    ["zj","t"],
+    ["zj","d"],
+
+    ["sz","t"],
+    ["sz","p"],
+    ["sz","d"],
+    ["sz","g"],
+    ["sz","c"],
+
+    ["l","t"],
+    ["l","p"],
+    ["l","s"],
+    ["l","z"],
+    ["l","d"],
+    ["l","f"],
+    ["l","g"],
+    ["l","zj"],
+    ["l","sz"],
+    ["l","x"],
+    ["l","c"],
+    ["l","b"],
+    ["l","n"],
+    ["l","m"],
+
+    ["x","t"],
+    ["x","d"],
+    ["x","c"],
+
+    ["c","t"],
+    ["c","s"],
+    ["c","z"],
+    ["c","d"],
+    ["c","sz"],
+
+    ["v","t"],
+    ["v","s"],
+    ["v","z"],
+    ["v","d"],
+    ["v","f"],
+    ["v","zj"],
+    ["v","sz"],
+    ["v","x"],
+    ["v","c"],
+
+    ["b","t"],
+    ["b","p"],
+    ["b","s"],
+    ["b","z"],
+    ["b","d"],
+    ["b","f"],
+    ["b","sz"],
+    ["b","l"],
+    ["b","x"],
+    ["b","c"],
+    ["b","v"],
+    ["b","n"],
+    ["b","m"],
+
+    ["n","t"],
+    ["n","s"],
+    ["n","z"],
+    ["n","d"],
+    ["n","f"],
+    ["n","g"],
+    ["n","zj"],
+    ["n","sz"],
+    ["n","x"],
+    ["n","c"],
+    ["n","v"],
+
+    ["m","t"],
+    ["m","p"],
+    ["m","s"],
+    ["m","z"],
+    ["m","d"],
+    ["m","f"],
+    ["m","g"],
+    ["m","zj"],
+    ["m","sz"],
+    ["m","x"],
+    ["m","c"],
+    ["m","v"],
+    ["m","b"],
+]
+
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
@@ -13,28 +293,29 @@ function init() {
 
     glyphSpace.addEventListener("click", () => {
         glyphSpace.textContent = "";
-        prompt = generatePrompt();
+        prompt = generatePrompt("block");
         for (const syllable of prompt.syllables) {
-            glyphSpace.appendChild(buildComplete(syllable));
+            glyphSpace.appendChild(buildSyllable(syllable));
         }
+        input.value = ""
         }
     );
 
         button.addEventListener('click',() => {
-            if (prompt.romanization === input.value.replace(/ /g,'')) {
+            if (prompt.romanization === parseRomanization(input.value)) {
                 feedback.textContent = '';
                 feedback.appendChild(buildComplete({open: ['y'], vowel: ['e'], close: ['s']}))
                 feedback.style.backgroundColor = '#90d390';
             } else {
                 feedback.textContent = '';
-                feedback.appendChild(buildComplete({open: ['n'], vowel: ['o'], close: ['p']}))
+                feedback.appendChild(buildNoClose({open: ['n'], vowel: ['o']}))
                 feedback.style.backgroundColor = '#fca1a1';
             }
         })
 
 };
 
-function generatePrompt() {
+function generatePrompt(method) {
 
     var loops = null;
     var random = randomInteger(1,10);
@@ -58,7 +339,7 @@ function generatePrompt() {
     var syllables = [];
 
     for (var i = 0; i < loops; i++) {
-        syllables.push(generateSyl());
+        syllables.push(generateSyl(method));
     }
 
     var romanization = romanize(syllables);
@@ -70,67 +351,101 @@ function generatePrompt() {
 
 }
 
-function generateSyl() {
+function generateSyl(method) {
 
-    var loops = null;
-    var random = randomInteger(1,10);
-    switch (true) {
-        /* case random > 8:
-            loops = 3;
-            break; */
-        case random > 5:
-            loops = 2;
-            break;
-        default:
-            loops= 1;
+    if (method === "phonemic") {
+
+        var loops = null;
+        var random = randomInteger(1,10);
+        switch (true) {
+            /* case random > 8:
+                loops = 3;
+                break; */
+            case random > 5:
+                loops = 2;
+                break;
+            default:
+                loops= 1;
+        }
+
+        var open = [];
+        for (var i = 0; i < loops; i++) {
+            open.push(consonants[randomInteger(0,(consonants.length - 1))])
+        }
+
+        if (randomInteger(1,10) > 7) loops = 2;
+        else {
+            loops = 1;
+        }
+
+        var vowel = [vowels[randomInteger(0,(vowels.length - 1))]];
+
+        /* for (var i = 0; i < loops; i++) {
+            vowel.push(
+                vowels[randomInteger(0,(vowels.length - 1))]
+            );
+        } */
+
+        random = randomInteger(1,10);
+        switch (true) {
+            /* case random > 8:
+                loops = 3;
+                break; */
+            case random > 5:
+                loops = 2;
+                break;
+            default:
+                loops= 1;
+        }
+
+        var close = [];
+        for (var i = 0; i < loops; i++) {
+            close.push(
+                consonants[randomInteger(0,(consonants.length - 1))]
+            );
+        }
+
+        return {
+            open: [...open],
+            vowel: [...vowel],
+            close: [...close]
+        }
     }
 
-    var open = [];
-    for (var i = 0; i < loops; i++) {
-        open.push(consonants[randomInteger(0,(consonants.length - 1))])
+    if (method === "block") {
+        var open = [];
+        var random = randomInteger(0,92);
+
+        if (random < 19) open = [...openConBlocks[random]];
+        else open = [...openConBlocks[random - 19]];
+
+        var vowel = [];
+        random = randomInteger(0,26);
+
+        if (random < 9) vowel = [...vowelBlocks[random]];
+        else vowel = [...vowelBlocks[random - 9]];
+
+        var close = [];
+
+        if (randomInteger(0,1)) {
+            random = randomInteger(0,148);
+
+            if (random < 18) close = [...closeConBlocks[random]];
+            else close = [...closeConBlocks[random - 18]];
+        }
+        
+
+        return {
+            open: [...open],
+            vowel: [...vowel],
+            close: [...close]
+        }
     }
-
-    if (randomInteger(1,10) > 7) loops = 2;
-    else {
-        loops = 1;
-    }
-
-    var vowel = [vowels[randomInteger(0,(vowels.length - 1))]];
-
-    /* for (var i = 0; i < loops; i++) {
-        vowel.push(
-            vowels[randomInteger(0,(vowels.length - 1))]
-        );
-    } */
-
-    random = randomInteger(1,10);
-    switch (true) {
-        /* case random > 8:
-            loops = 3;
-            break; */
-        case random > 5:
-            loops = 2;
-            break;
-        default:
-            loops= 1;
-    }
-
-    var close = [];
-    for (var i = 0; i < loops; i++) {
-        close.push(
-            consonants[randomInteger(0,(consonants.length - 1))]
-        );
-    }
-
-    return {
-        open: [...open],
-        vowel: [...vowel],
-        close: [...close]
-    }
+    
 }
 
 function buildComplete({ open, vowel, close }) {
-    const glyphDiv = e("div",["syllable"]);
+    const glyphDiv = e("div",["complete-syllable"]);
     glyphDiv.appendChild(e("div",["bound"]));
 
     const lateralPanels = glyphDiv.appendChild(e("div",["complete-lateral-panels"]));
@@ -157,6 +472,54 @@ function buildComplete({ open, vowel, close }) {
         img.src = `resources/symbols\ short/${phoneme}.png`;
         bottomPanel.appendChild(img);
     })
+
+    return glyphDiv;
+}
+
+function buildNoClose({ open, vowel }) {
+    const glyphDiv = e("div",["no-close-syllable"]);
+    glyphDiv.appendChild(e("div",["bound"]));
+    const lateralPanels = glyphDiv.appendChild(e("div",["no-close-lateral-panels"]));
+
+    const leftPanel = lateralPanels.appendChild(e("div",["no-close-left-panel"]));
+    open.forEach((phoneme) => {
+        const img = e("img",["phoneme"]);
+        img.src = `resources/symbols\ tall/${phoneme}.png`;
+        leftPanel.appendChild(img);
+    })
+
+    const rightPanel = lateralPanels.appendChild(e("div",["no-close-right-panel"]));
+    const img = e("img",["phoneme"]);
+    img.src = `resources/symbols\ tall/${vowel[0]}.png`;
+    rightPanel.appendChild(img);
+
+    return glyphDiv;
+}
+
+function buildNoCloseDoubleVowel({ open, vowel }) {
+    const glyphDiv = e("div",["ncdv-syllable"]);
+    glyphDiv.appendChild(e("div",["bound"]));
+
+    const lateralPanels = glyphDiv.appendChild(e("div",["ncdv-lateral-panels"]));
+    const leftPanel = lateralPanels.appendChild(e("div",["ncdv-left-panel"]));
+    open.forEach(phoneme => {
+        const img = e("img",["phoneme"]);
+        img.src = `resources/symbols\ tall/${phoneme}.png`;
+        leftPanel.appendChild(img);
+    })
+
+    const rightPanel = lateralPanels.appendChild(e("div",["ncdv-right-panel"]));
+
+    const topPanel = rightPanel.appendChild(e("div",["ncdv-right-top-panel"]));
+    const imgTop = e("img",["phoneme"]);
+    imgTop.src = `resources/symbols\ short/${vowel[0]}.png`;
+    topPanel.appendChild(imgTop);
+
+    rightPanel.appendChild(e("div",["bound"]));
+    const bottomPanel = rightPanel.appendChild(e("div",["ncdv-right-bottom-panel"]));
+    const imgBottom = e("img",["phoneme"]);
+    imgBottom.src = `resources/symbols\ short/${vowel[1]}.png`;
+    bottomPanel.appendChild(imgBottom);
 
     return glyphDiv;
 }
@@ -195,4 +558,51 @@ function randomInteger(minimum, maximum) {
 	return Math.floor(
 		(Math.random() * (maximum - minimum + 1)) + minimum
 	);
+}
+
+function identifySylType({open, vowel, close}) {
+    var sylType = "error";
+
+    if (open.length &&
+        vowel.length &&
+        close.length) sylType = "complete";
+
+    if (open.length &&
+        (vowel.length === 1) &&
+        !close.length) sylType = "no close";
+
+        if (open.length &&
+            (vowel.length === 2) &&
+            !close.length) sylType = "no close double vowel";
+
+    if (!open.length &&
+        vowel.length &&
+        close.length) sylType = "no open";
+
+    if (!open.length &&
+        (vowel.length === 1) &&
+        !close.length) sylType = "vowel";
+
+    if (!open.length &&
+        (vowel.length === 2) &&
+        !close.length) sylType = "double vowel";
+
+    if (!vowel.length &&
+        (open.length || close.length)) sylType = "consonant";
+
+    return sylType;
+}
+
+function buildSyllable(syllable) {
+    const sylType = identifySylType(syllable);
+
+    if (sylType === "complete") return buildComplete(syllable);
+    if (sylType === "no close") return buildNoClose(syllable);
+    if (sylType === "no close double vowel") return buildNoCloseDoubleVowel(syllable);
+}
+
+function parseRomanization(text) {
+    text = text.replace(/ /g,'');
+    text = text.replace(/(q|k)/gi,'c');
+    return text.toLowerCase();
 }
